8b2ae99697e8e920ca7c022ab911d31bb6570dc6
Brian <a@atomrigs.io>
2020-12-31 18:28:39 -0600

    infura settubg change

--- src/cron/index.js
+++ src/cron/index.js
@@ -11,7 +11,10 @@ require('dotenv').config({ path: require('find-config')('.env') })
 
 const network = 'kovan'
 const infuraId =  process.env.INFURA_API_KEY
-const host = `https://kovan.infura.io/v3/${infuraId}`
+const infuraSecret = process.env.INFURA_SECRET
+const host = `https://:${infuraSecret}@${network}.infura.io/v3/${infuraId}`
+const wshost = `wss://:${infuraSecret}@${network}.infura.io/ws/v3/${infuraId}`
+//const host = `https://kovan.infura.io/v3/${infuraId}`
 //const host = `wss://kovan.infura.io/ws/v3/${infuraId}`
 
 let pollAddr
